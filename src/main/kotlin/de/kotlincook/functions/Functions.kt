2742f8939c6e75a23d7680f7fdd051009d06cf4e
javacook <javacook@gmx.de>
2018-09-20 17:27:14 +0200

    Zwischenstand

--- src/main/kotlin/de/kotlincook/functions/Functions.kt
+++ src/main/kotlin/de/kotlincook/functions/Functions.kt
@@ -1,64 +1,30 @@
-package functions
+package de.kotlincook.functions
 
-import java.io.Closeable
-import java.io.StringReader
+fun doubler1(x: Double): Double {
+    return 2 * x
+}
+
+fun doubler2(x: Double) = 2 * x
+
+var doubler1 = fun (x: Double): Double {
+    return 2 * x
+}
+
+var doubler2 = fun (x: Double) = 2 * x
+
+var doubler3: (Double) -> Double = {x -> 2 * x}
+
+var doubler4 =  {x: Double -> 2 * x}
+
+// TODO : Unterschied Annonym Funktion und Lamda
 
 val squared = {x: Double -> x*x}
 
 val reciprocal = fun (x: Double) = 1.0 / x
 
-val doubler = fun (n: Double) = 2 * n
-
-//fun doubler(x: Int) = 2 * x
-
-
-fun executeOneToTen(n: Int, lambda: (Int) -> Int) {
-    for (i in 1..10) lambda(i)
-}
-
-fun using(obj: Closeable, block: () -> Unit) {
-    try {
-        block()
-    } finally {
-        obj.close()
-    }
-}
-
-fun stopwatch(function: () -> Unit): Long {
-    val start = System.currentTimeMillis()
-    function()
-    return System.currentTimeMillis() - start
-
-}
-
-fun parallel(block: () -> Unit) {
-    val thread =  object : Thread() {
-        override fun run() {
-            block()
-        }
-    }
-    thread.start()
-}
-
-
+val doubler = fun (x: Double) = 2 * x
 
 
 fun main(args: Array<String>) {
-    executeOneToTen(4) {n -> 2 * n}
-    val reader = StringReader("Hallo")
-    using(reader) {
-        println(reader.read())
-    }
-    parallel {
-        println("Hello")
-    }
-    val period = stopwatch {
-        for (i in 0..100000) i
-    }
-    println(period)
-
-    doubler(reciprocal(4.0))
-
-//    val comp = {x:Double -> doubler(reciprocal(x))}
-
+    val comp = {x:Double -> doubler(reciprocal(x))}
 }
--- src/main/kotlin/de/kotlincook/functions/HigherOrder.kt
+++ src/main/kotlin/de/kotlincook/functions/HigherOrder.kt
@@ -1,2 +1,51 @@
 package de.kotlincook.functions
 
+import java.io.Closeable
+import java.io.StringReader
+
+
+fun executeOneToTen(lambda: (Double) -> Double) {
+    for (i in 1..10) println(lambda(i.toDouble()))
+}
+
+fun using(obj: Closeable, block: () -> Unit) {
+    try {
+        block()
+    } finally {
+        obj.close()
+    }
+}
+
+fun stopwatch(function: () -> Unit): Long {
+    val start = System.currentTimeMillis()
+    function()
+    return System.currentTimeMillis() - start
+
+}
+
+fun thread(block: () -> Unit) {
+    val thread =  object : Thread() {
+        override fun run() {
+            block()
+        }
+    }
+    thread.start()
+}
+
+fun main(args: Array<String>) {
+    executeOneToTen(doubler)
+
+
+    val reader = StringReader("Hallo")
+    using(reader) {
+        println(reader.read())
+    }
+    thread {
+        println("Hello")
+    }
+    val period = stopwatch {
+        for (i in 0..100000) i
+    }
+    println(period)
+
+}
