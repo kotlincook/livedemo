c4c5096be611cf1fc349d1938a31a7cb4a95e24a
javacook <javacook@gmx.de>
2018-09-20 17:22:34 +0200

    initial

--- src/main/kotlin/de/kotlincook/coroutines/Actors.kt
+++ src/main/kotlin/de/kotlincook/coroutines/Actors.kt
@@ -1,2 +1,36 @@
-package de.kotlincook.coroutines
+package de.kotlincook.coroutines.acotors
 
+import de.kotlincook.coroutines.acotors.CounterMessage.GetCounter
+import de.kotlincook.coroutines.acotors.CounterMessage.IncCounter
+import kotlinx.coroutines.experimental.CompletableDeferred
+import kotlinx.coroutines.experimental.channels.actor
+import kotlinx.coroutines.experimental.runBlocking
+
+sealed class CounterMessage {
+    object IncCounter: CounterMessage()
+    class GetCounter(val response: CompletableDeferred<Int>): CounterMessage()
+}
+
+
+fun counterActor() = actor<CounterMessage> {
+    var counter = 0
+    for (msg in channel) {
+        when (msg) {
+            is IncCounter -> counter++
+            is GetCounter -> msg.response.complete(counter)
+        }
+    }
+}
+
+
+
+fun main(args: Array<String>) = runBlocking {
+    val counter = counterActor()
+    repeat(1000) {
+        counter.send(IncCounter)
+    }
+    val response = CompletableDeferred<Int>()
+    counter.send(GetCounter(response))
+    println(response.await())
+    // counter.close()
+}
--- src/main/kotlin/de/kotlincook/coroutines/Pipelines.kt
+++ src/main/kotlin/de/kotlincook/coroutines/Pipelines.kt
@@ -1,2 +1,25 @@
 package de.kotlincook.coroutines
 
+import kotlinx.coroutines.experimental.channels.ReceiveChannel
+import kotlinx.coroutines.experimental.channels.produce
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.runBlocking
+
+fun producePipeline() = produce {
+    var counter = 0;
+    while(true) send(counter++)
+}
+
+fun consumePipeline(numbers: ReceiveChannel<Int>) = produce {
+    for (num in numbers) send(num * num)
+}
+
+fun main(args: Array<String>) = runBlocking {
+    val numbers = producePipeline()
+    val squares = consumePipeline(numbers)
+    for (i in 1..50) {
+        println(squares.receive())
+        delay(100)
+    }
+
+}
