fbd91c34f56a63ef29da40ba301d3815425297df
Jörg Vollmer <javacook@gmx.de>
2019-02-15 08:57:40 +0100

    Update auf Koroutinen 1.3

--- src/main/kotlin/de/kotlincook/quicksort/Quicksort.kt
+++ src/main/kotlin/de/kotlincook/quicksort/Quicksort.kt
@@ -1,18 +1,30 @@
 package de.kotlincook.quicksort
 
 import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
 import java.util.concurrent.CompletableFuture
 
-suspend fun <T: Comparable<T>> List<T>.coroutSort(): List<T> {
-    println(Thread.currentThread())
-    return if (size <= 1) this
+//suspend fun <T: Comparable<T>> List<T>.coroutSort(): List<T> {
+//    println(Thread.currentThread())
+//    return if (size <= 1) this
+//    else {
+//        val pivot = first()
+//        val (lower, higher) = drop(1).partition { it <= pivot }
+//        val lowFut = async { lower.coroutSort() }
+//        val highFut = async { higher.coroutSort() }
+//        lowFut.await() + pivot + highFut.await()
+//    }
+//}
+
+suspend fun <T: Comparable<T>> List<T>.quicksort(): List<T> = coroutineScope {
+    if (size <= 1) this@quicksort
     else {
         val pivot = first()
-        val (lower, higher) = drop(1).partition { it <= pivot }
-        val lowFut = async { lower.coroutSort() }
-        val highFut = async { higher.coroutSort() }
-        lowFut.await() + pivot + highFut.await()
+        val (left, right) = drop(1).partition { it < pivot }
+        val deferredLeft = async { left.quicksort() }
+        val deferredRight = async { right.quicksort() }
+        deferredLeft.await() + pivot + deferredRight.await()
     }
 }
 
@@ -35,5 +47,5 @@ fun <T: Comparable<T>> List<T>.completeSort(): List<T> {
 
 fun main(args: Array<String>) = runBlocking {
     val list = listOf(5, 2, 8, 5, 4, 7, 3, 5, 4, 6)
-    println(list.sorted() == list.coroutSort())
+    println(list.sorted() == list.quicksort())
 }
